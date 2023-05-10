21f2604e5b88ea233021d78cd5c53bc7c9c9619e
naveenmech <vas.naveenkumar99@gmail.com>
2023-05-10 12:22:17 +0530

    add image in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
         "hero-image":
           "url('https://wallpapers.com/images/featured/hdj6cfehppy286jt.jpg')",
         "hero-image2":
-          "url('https://images.unsplash.com/photo-1597316773259-bfe49466e46c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE1fHx8ZW58MHx8fHw%3D&w=1000&q=80')",
+          "url('https://images.unsplash.com/photo-1494145904049-0dca59b4bbad?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8YnVpbGRpbmd8ZW58MHx8MHx8&auto=format&fit=crop&w=700&q=60')",
       },
       // screens: {
       //   sm: { min: "640px", max: "767px" },
